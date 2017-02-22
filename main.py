4a1800ad39423662621dc396dc865a482de94313
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-02-22 22:27:08 +0900

    GET /threads/create

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/threads/create', methods=['GET', 'POST'])
+def create_thread():
+    if request.method == 'GET':
+        return render_template('create_thread.html')
+    elif request.method == 'POST':
+        return redirect(url_for('create_thread'))
 
 
 if __name__ == '__main__':
